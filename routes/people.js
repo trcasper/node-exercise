cfbb55e4e29c908c9adce72821589cc4df737133
trcasper <trcasper90@gmail.com>
2020-11-28 15:06:25 -0700

    all people

--- routes/people.js
+++ routes/people.js
@@ -1,13 +1,58 @@
 var express = require('express');
 var router = express.Router();
-var http = require('http');
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  request({
-    uri: 'https://swapi.dev/api/people/'
-  }).pipe(res)
-  // res.send('peoplepage');
+  const uri = 'https://swapi.dev/api/people';
+
+  async function getPeople(pageNumber) {
+    console.log(`Calling api for page ${pageNumber}`)
+    const url = `${uri}/?page=${pageNumber}`;
+    console.log(url);
+    return await axios.get(url);
+  }
+
+  async function getAllPeople() {
+    let records = [];
+  
+    let totalPages = 10;
+
+    for(i = 1; i < totalPages; i++) {
+      let response = await getPeople(i);
+      console.log(response.data);
+      records = records.concat(response.data);
+    }
+
+    res.send(records);
+  
+
+  // do {
+  //   let { data: response } = await getPeople(page);
+  //   console.log(totalPages);
+  //   records = records.concat(response.data);
+  //   page++;
+  // } while (page < totalPages)
+  }
+
+
+  getAllPeople();
+
 });
 
 module.exports = router;
+
+  //pulls the first page//
+  // const getPeople = async () => {
+  //   try {
+  //     return await axios.get(uri);
+  //   } catch(error)
+  //       {
+  //     console.error(error);
+  //   }
+  //     }
+      
+  //     getPeople().then((response) => {
+  //       const data = response.data;
+  //       res.send(data);
+  //     })
--- routes/planets.js
+++ routes/planets.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('planetpage');
+  const uri = 'https://swapi.dev/api/planets/';
+
+  const getPlanets = async () => {
+    try {
+      return await axios.get(uri);
+    } catch(error)
+    {
+      console.error(error);
+    }
+  }
+
+  getPlanets().then((response) => {
+    const data = response.data;
+    res.send(data);
+  })
 });
 
 module.exports = router;

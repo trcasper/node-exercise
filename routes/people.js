e8a28197a242a591d5897ffcda67d82c138e615f
trcasper <trcasper90@gmail.com>
2020-11-28 13:05:15 -0700

    people-push

--- routes/people.js
+++ routes/people.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('peoplepage');
+  request({
+    uri: 'https://swapi.dev/api/people/'
+  }).pipe(res)
+  // res.send('peoplepage');
 });
 
 module.exports = router;
